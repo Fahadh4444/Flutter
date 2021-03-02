1cb1fe82611f587c20eb65e11dd7cf6f05f8e4c5
Kasala Fahadh <67508953+Fahadh4444@users.noreply.github.com>
2021-03-03 02:15:10 +0530

    Created "Destiny The Adventure" app using Flutter.

---
+++ Udemy Angela/Challenges/destiny_the_adventure_game/README.md
@@ -0,0 +1,3 @@
+## Challenge is to make Adventure App as shown below
+
+![Adventure App](./Adventure%20App.jpeg)
---
+++ Udemy Angela/Challenges/destiny_the_adventure_game/android/app/src/main/kotlin/com/kasalafahadh/destiny_the_adventure_game/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kasalafahadh.destiny_the_adventure_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
